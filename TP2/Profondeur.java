27cb0969d16995479cce9f4da89b874d4758e75f
ChristianHasbani <Christian_Hasbani@etu.u-bourgogne.fr>
2023-03-18 19:26:52 +0100

    updated TP2

--- TP2/Profondeur.java
+++ TP2/Profondeur.java
@@ -8,14 +8,14 @@ public class Profondeur extends LC0_Algorithm {
     @Override
     public String getDescription() {
         return "Spanning tree algorithm using LCO. \n" +
-                "Rule 1: N--A --> A--M \n" + 
-                "Rule 2: A--M --> F--A";
+                "Rule 1: N --- A --> A x---x M \n" + 
+                "Rule 2: A x---x M --> F x---x A";
     }
 
     @Override
     protected void beforeStart() {
         setLocalProperty("label", vertex.getLabel()); //Set the label property for each node
-        setLocalProperty("parent", -1); //Set the parent for all nodes as -1
+        setLocalProperty("parent", -1); //Set the parent port to -1 to remember later the port that is the start of all the neighbors
         this.neighborStates = new String[vertex.getDegree()]; // get the number of neighbor nodes 
         for(int i = 0; i < vertex.getDegree(); i++){
             this.neighborStates[i]="N"; //Set all neighbor states to N by default
@@ -24,17 +24,19 @@ public class Profondeur extends LC0_Algorithm {
 
     @Override
     protected void onStarCenter() {
-        this.neighborStates[neighborDoor] = getNeighborProperty("label").toString(); //Update the neighbor states for this node
+        this.neighborStates[neighborDoor] = getNeighborProperty("label").toString(); //Update the neighbor states for the node rewriting the rules
         R1();
         R2();
     }
 
     // Function for rule 1
     private void R1() {
-        if (getLocalProperty("label").equals("N") && getNeighborProperty("label").equals("A")) { //Check if rule 1 applies for those 2 nodes
-            setLocalProperty("label", "A");//If it applies then we change the node to status A and the neighbor to status M
-            setNeighborProperty("label", "M"); 
-            setLocalProperty("parent",neighborDoor); //The node that applies the rule saved the port on which its parent is located
+         //Check if rule 1 applies for those 2 nodes
+         //Check if the local node has a label "N" and the neighbor node has the label "A"
+        if (getLocalProperty("label").equals("N") && getNeighborProperty("label").equals("A")) {
+            setLocalProperty("label", "A");// Change the local node's label to A 
+            setNeighborProperty("label", "M"); // The neighbor's to M
+            setLocalProperty("parent",neighborDoor); //The node that applies the rule saved the port on which its parent is located to mark the link
             setDoorState(new MarkedState(true), neighborDoor); // Mark the link between those the local and neighbor node 
             this.neighborStates[neighborDoor] = getNeighborProperty("label").toString(); // Update the neighbors states array
         }
@@ -42,12 +44,13 @@ public class Profondeur extends LC0_Algorithm {
 
     // Function for rule 2
     private void R2() {
-        if(getLocalProperty("label").equals("A") && getNeighborProperty("label").equals("M")){ //Verify that rule 2 applies for this node and its neighbor
-            // We check if the node that applies the rule is indeed a node with state "A" and its neighbor with "M"
-            if(neighborDoor == (int) getLocalProperty("parent")){ 
+         //Verify that rule 2 applies for this node and its neighbor
+         // Check if the local node has the label "A" and the neighbor node has the label "M"
+        if(getLocalProperty("label").equals("A") && getNeighborProperty("label").equals("M")){
+            if(neighborDoor == (int) getLocalProperty("parent")){ // Check if the link is marked
                 if(!neighborsExist()){ //There isn't any neighbors with the state "N"
-                    setLocalProperty("label","F");
-                    setNeighborProperty("label","A");
+                    setLocalProperty("label","F"); //Change the label of the local node
+                    setNeighborProperty("label","A"); // Chagne the label of the neighbor node
                 }
             }
         }
